29e6157ad155dd5a54085fe43247e2ce45adf3dc
Martin Fenner <martin@front-matter.io>
2023-05-09 18:32:44 +0200

    Update init.sh

--- invenio/init.sh
+++ invenio/init.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 
 # run all pending database migrations
+# invenio db init create
 invenio alembic upgrade heads
+
+# set location for file uploads
+invenio files location create data /opt/invenio/var/instance/data --default
+
+# enable custom fields
+invenio rdm-records custom-fields init
+invenio communities custom-fields init
 
 # import fixtures
 invenio rdm-records fixtures
+invenio rdm fixtures
+
+# refresh opensearch indices
+invenio index destroy --force --yes-i-know
+invenio index init
+invenio rdm-records rebuild-index
+invenio communities rebuild-index
+
+# Steps to declare the MQ queues required for statistics, etc.
+invenio queues declare
 
 # create demo records
 invenio rdm-records demo communities
 invenio rdm-records demo records
 
-# refresh opensearch indices
-invenio index destroy --force --yes-i-know
-invenio index init
+# add user with admin role
+invenio roles create admin
+invenio access allow superuser-access role admin
+# invenio users create <email> --active
+# invenio roles add <email> admin
